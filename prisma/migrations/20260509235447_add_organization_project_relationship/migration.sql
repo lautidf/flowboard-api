26191fc80c3a3017dd6a75492753d903e0c8bf14
Lautaro De Filippo <lautimax@gmail.com>
2026-05-11 13:32:42 -0300

    run migration for latest schema

---
+++ prisma/migrations/20260509235447_add_organization_project_relationship/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `organizationId` to the `Project` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "organizationId" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
