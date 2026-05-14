107b484a7c9c952a32ee8945f94244e9d38ccb45
Lautaro De Filippo <lautimax@gmail.com>
2026-05-14 18:28:34 -0300

    refactor organizationService.getOne request type

--- src/modules/organizations/organization.controller.ts
+++ src/modules/organizations/organization.controller.ts
@@ -5,7 +5,7 @@ export async function create(req: Request, res: Response) {
 	const { name } = req.body;
 	const userId = req.user.id;
 
-	const organization = await organizationService.create(name, userId);
+	const organization = await organizationService.create({ name, userId });
 
 	res.status(201).json(organization);
 };
@@ -18,8 +18,8 @@ export async function getAll(req: Request, res: Response) {
 	res.json(organizations);
 }
 
-type GetOneRequest = Request<{ id: string }>;
-export async function getOne(req: GetOneRequest, res: Response) {
+type GetOneParams = { id: string };
+export async function getOne(req: Request<GetOneParams>, res: Response) {
 	const { id } = req.params;
 	const userId = req.user.id;
 
