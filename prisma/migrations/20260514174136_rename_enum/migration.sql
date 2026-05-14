be47ceceb2048b0457173e42c925f069389707ba
Lautaro De Filippo <lautimax@gmail.com>
2026-05-14 14:42:18 -0300

    rename enum

---
+++ prisma/migrations/20260514174136_rename_enum/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - Changed the type of `role` on the `Membership` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- CreateEnum
+CREATE TYPE "MembershipRole" AS ENUM ('MEMBER', 'OWNER');
+
+-- AlterTable
+ALTER TABLE "Membership" DROP COLUMN "role",
+ADD COLUMN     "role" "MembershipRole" NOT NULL;
+
+-- DropEnum
+DROP TYPE "OrganizationRole";
