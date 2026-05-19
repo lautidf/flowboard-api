09890280ea8f4e64c05a5fd9f2ec33f4dcb8164f
Lautaro De Filippo <lautimax@gmail.com>
2026-05-19 14:29:17 -0300

    create base class for app errors

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -1,5 +1,24 @@
-export class ConflictError extends Error {}
+export abstract class AppError extends Error {
+	constructor(message: string, readonly statusCode: number) {
+		super(message);
+		Error.captureStackTrace(this, this.constructor);
+	}
+}
 
-export class NotFoundError extends Error {}
+export class ConflictError extends AppError {
+	constructor(message: string) {
+		super(message, 409);
+	}
+}
 
-export class ForbiddenError extends Error {}
+export class NotFoundError extends AppError {
+	constructor(message: string) {
+		super(message, 404);
+	}
+}
+
+export class ForbiddenError extends AppError {
+	constructor(message: string) {
+		super(message, 403);
+	}
+}
--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,22 +1,15 @@
 import { ErrorRequestHandler, NextFunction, Request } from 'express';
 import { logger } from '../lib/logger';
-import { ConflictError, NotFoundError } from '../errors/errors';
+import { AppError, ConflictError, NotFoundError } from '../errors/errors';
 
 export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+	if (err instanceof AppError) {
+		return res.status(err.statusCode).json({
+			message: err.message,
+		});
+	}
+	
 	logger.error(err);
-
-	if (err instanceof ConflictError) {
-		return res.status(409).json({
-			message: err.message,
-		});
-	}
-
-	if (err instanceof NotFoundError) {
-		return res.status(404).json({
-			message: err.message,
-		});
-	}
-
 	res.status(500).json({
 		message: 'Internal server error'
 	});
