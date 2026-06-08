e176f8b8db2636f1621800339f64be2f2807b759
Lautaro De Filippo <lautimax@gmail.com>
2026-06-08 10:39:09 -0300

    feat: add request validation for organization creation

--- src/modules/organizations/organization.controller.ts
+++ src/modules/organizations/organization.controller.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from 'express';
 import { organizationService } from './organization.service';
 import {
+	createRequestSchema,
 	getAllRequestSchema,
 	getOneRequestSchema,
 	removeRequestSchema
 } from './organization.schemas';
 
 export async function create(req: Request, res: Response) {
-	const { name } = req.body;
+	const { body } = createRequestSchema.parse({ body: req.body });
+	
+	const { name } = body;
 	const userId = req.user.id;
 
 	const organization = await organizationService.create({ name, userId });
